3ebe6821b4652279335b29b40bca37091cd086d9
Roshan Roy <roshanroy1121@gmail.com>
2025-10-10 11:23:09 +0530

    useState()

--- react-state/react-state/src/Counter.jsx
+++ react-state/react-state/src/Counter.jsx
@@ -1,15 +1,17 @@
+import { useState } from "react";
+
 function Counter() {
-  let count = 0;
+  let [count, setCount] = useState(0);
 
   function incrementCounter() {
-    count = count + 1;
+    setCount(count + 1);
     console.log(count);
   }
 
   return (
     <div>
       <h3>Count = {count}</h3>
-      <button onClick={incrementCounter}>Increase Count</button>
+      <button onClick={incrementCounter}>Increment Count</button>
     </div>
   );
 }
